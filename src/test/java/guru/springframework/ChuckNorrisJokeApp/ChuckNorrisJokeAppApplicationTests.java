c9c9c37f9dee25554d089157f465b9f7d493134d
Rory O'Connor <gonzooo@gmail.com>
2023-08-10 20:58:07 +1000

    Add Spring Initializr code, closes #1

---
+++ src/test/java/guru/springframework/ChuckNorrisJokeApp/ChuckNorrisJokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.ChuckNorrisJokeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
